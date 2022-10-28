5c627bfdad27746b8c1dd7603a9893a749dcbc91
Keiler Trindade <keiler.trindade@hotmail.com>
2022-10-28 18:30:44 -0300

    Adicionado código - Atividade substitutiva

--- contagem_regressiva.py
+++ contagem_regressiva.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
+for andar in range (20, 0, -1):
+  if andar != 13:
+    print(andar)
+    
+    
+andar = 20
+while andar > 0:
+  if andar != 13:
+    print(andar)
+  andar -= 1
 
+-------------------------------
 """Faça um código que execute a contagem regressiva de uma bomba, informando o número de segundos para explodir.
 Ele deverá mostrar a mensagem “iniciando contagem regressiva”, os segundos passados e, no final, a mensagem “BUM!”.
 Não é necessário, mas, caso deseje tornar o sistema mais realista para que o tempo realmente passe em segundos, você pode usar a função time.sleep() que existe na Python (acesse o link em anexo). 
